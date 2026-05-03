11a2490fde350703e5652f08360142934696f9c9
Ibrahim-Mohamed-66 <ibrahim.mohamed6005@gmail.com>
2026-05-03 22:37:47 +0300

    chore(migrations): add RemoveRedundentColumnsInStudentAndInstructor migration

---
+++ Nilearn.Infrastructure/Migrations/20260503110042_RemoveRedundentColumnsInStudentAndInstructor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nilearn.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRedundentColumnsInStudentAndInstructor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_StudentNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentLevel",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "EarningBalance",
+                table: "Instructors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentLevel",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentNumber",
+                table: "Students",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EarningBalance",
+                table: "Instructors",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StudentNumber",
+                table: "Students",
+                column: "StudentNumber",
+                unique: true);
+        }
+    }
+}
